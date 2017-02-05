fc2d874416334101ca61f3c430a218f510fff69b
Mei Yang Indah Sucianti (14111145) <meiiindah91@gmail.com>
2017-02-05 19:23:38 +0700

    Add files via upload

---
+++ Tugas1 web 2/codingsederhana.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Contoh Coding PHP Sederhana</title>
+</head>
+
+<body>
+ <form method = 'post' action= "aksiForm.php">
+  <table>
+   <tr>
+    <td>Nama</td>
+    <td>:</td>
+    <td><input type='text' name='nama'></td>
+   </tr>
+   <tr>
+    <td>Umur</td>
+    <td>:</td>
+    <td><input type='text' name='umur'></td>
+   </tr>
+   <tr>
+    <td>Alamat</td>
+    <td>:</td>
+    <td><input type='text' name='alamat'></td>
+   </tr>
+   <tr>
+    <td></td>
+    <td></td>
+    <td><input type='submit' value='KIRIM'></td>
+   </tr>
+  </table>  
+ </form>
+</body>
+</html>
